b77174f734a08981f2613fd5ca44ffbdd926550d
Eony5 <jonathancodas@gmail.com>
2017-02-16 16:47:14 +0100

    hello

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
 
   $(".right").click(function(){
+    $(".left").removeClass('return');
+    $(".right").removeClass('return');
+
     $(".left").addClass("fade-left");
     $(".right").addClass("fade-right");
+
 
     $(".foot-container").
     css("transition","0.5s").
@@ -18,6 +22,9 @@ $(document).ready(function(){
   });
 
   $(".left").click(function(){
+    $(".left").removeClass('return');
+    $(".right").removeClass('return');
+
     $(".left").addClass("fade-left");
     $(".right").addClass("fade-right");
 
@@ -32,5 +39,25 @@ $(document).ready(function(){
       css("transform","scale(1)").
       css("opacity","1");
     },1000);
+  });
+
+  $(".arrow").click(function() {
+    $(".foot-container, .basket-container").
+    css("transition","0.5s").
+    css("opacity","0");
+
+    setTimeout(function(){
+      $(".foot-container, .basket-container").
+      css("transition","1s").
+      css("transform","scale(0)").
+      css("opacity","0");
+    },1000);
+
+    setTimeout(function (){
+      $(".left").addClass('return');
+      $(".right").addClass('return');
+
+      $(".foot-container, .basket-container").css("display","none");
+    }, 500);
   });
 });
