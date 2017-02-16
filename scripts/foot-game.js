da197a07b090f4b1dd9cb32cf8564fd949ad6c9c
Eony5 <jonathancodas@gmail.com>
2017-02-16 12:16:58 +0100

    lmfao

--- scripts/foot-game.js
+++ scripts/foot-game.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
   //var kickSound = new Audio("ball-kick-sound.wav");
 
-  $("#ball").click(function(){
-    $("#ball").toggleClass("ball-shoot");
+  $(".ball").click(function(){
+    $(".ball").toggleClass("ball-shoot");
     kickSound.play();
 
     setTimeout(function(){
-      $("#ball").removeClass("ball-shoot");
+      $(".ball").removeClass("ball-shoot");
     },6000);
   });
 });
